14680aa70faa152855bc091c637530d61486f793
SDozH <soldatov_alex@bk.ru>
2022-07-25 03:50:46 +0300

    Добавьте файлы проекта.

---
+++ Unit9final/MyListException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unit9final;
+
+namespace Task1
+{
+    public class AgeMin : ArgumentException
+    {
+        public AgeMin(string message)
+            : base(message)
+        { }
+    }
+    public class NaneException : ArgumentException
+    {
+        public NaneException(string message)
+            : base(message)
+        { }
+    }
+    public class AgeMax : ArgumentException
+    {
+        public AgeMax(string message)
+            : base(message)
+        { }
+    }
+    public class PhoneEX : ArgumentException
+    {
+        public PhoneEX(string message)
+            : base(message)
+        { }
+    }
+}
+
---
+++ Unit9final/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using Task1;
+using System.Text.RegularExpressions;
+
+namespace Unit9final
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Student student = new Student { Name = "Tom", Age = 18, Phone = "+7921231212" };
+                if (student.Name == null || student.Name.Length < 2)
+                {
+                    throw new NaneException("Длина имени меньше 2 символов");
+                }
+                if (student.Age < 18)
+                {
+                    throw new AgeMin("Лицам до 18 регистрация запрещена");
+                }
+                if (student.Age > 23)
+                {
+                    throw new AgeMax("Ты слишком старый");
+                }
+                if (student.Phone == null || student.Phone.Length != 12)
+                {
+                    throw new PhoneEX("Не корректный номер");
+                }
+                Console.WriteLine("Добро пожаловать {0}", (student.Name));
+            }
+            catch (AgeMin ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (AgeMax ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (NaneException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (PhoneEX ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(NotSupportedException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Конец");
+            }
+        }
+
+
+        public class Student
+        {
+
+            public string Name { get; set; } = "";
+            public int Age { get; set; }
+            public string Phone { get; set; }
+        }
+    }
+}
+
+
+
+
